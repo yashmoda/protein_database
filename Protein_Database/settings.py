b1db825f90dcecc3126684e85a289b5a0e9431b0
Yash Moda <yashmoda07@gmail.com>
2020-05-06 21:24:20 +0000

    project running on pythonanywhere

--- Protein_Database/settings.py
+++ Protein_Database/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$#x%7%*bc)ujg3(rsuq-i)%470)xfh*7-a$1da*-sgt7r$4)!)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yashmoda.pythonanywhere.com']
 
 
 # Application definition
@@ -119,9 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR, "static"),
+# ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
